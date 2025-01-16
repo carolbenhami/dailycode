6b9a56c25ca0a270ca60a1befcf942ba5cea9dbf
carolbenhami <145382917+carolbenhami@users.noreply.github.com>
2025-01-16 09:09:03 -0500

    Update python_FindMissingNumber.py

--- python_FindMissingNumber.py
+++ python_FindMissingNumber.py
@@ -9,12 +9,15 @@ for i in range (1, list_size+1):
     i+=1
 
 to_be_removed = rand.randint(1,list_size)
+print (to_be_removed)
 
 original_list.remove(to_be_removed)
 
 print (f"List with number removed: {original_list}\n")
+def missingnum():
+    for i in range (1,list_size + 1):
+        if i not in original_list:
+            original_list.insert(i-1, i)
+            print (f"Appended List: {original_list}\n")
 
-for i in range (1,list_size + 1):
-    if i not in original_list:
-        original_list.insert(i-1, i)
-        print (f"Appended List: {original_list}\n")
+missingnum()
