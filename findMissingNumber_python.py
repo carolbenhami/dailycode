226c0e63a3aa761c3ecd42ad1ff0b5d4217a9ced
carolbenhami <145382917+carolbenhami@users.noreply.github.com>
2025-01-16 08:24:12 -0500

    Create findMissingNumber_python.py

---
+++ findMissingNumber_python.py
@@ -0,0 +1,20 @@
+import random as rand
+
+list_size = rand.randint(1,15)
+original_list = []
+
+i = 1
+for i in range (1, list_size+1):
+    original_list.append(i)
+    i+=1
+
+to_be_removed = rand.randint(1,list_size)
+
+original_list.remove(to_be_removed)
+
+print (f"List with number removed: {original_list}\n")
+
+for i in range (1,list_size + 1):
+    if i not in original_list:
+        original_list.insert(i-1, i)
+        print (f"Appended List: {original_list}\n")
